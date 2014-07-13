44778888c66df55c02b554e07aebfe15d1200735
macheteshuffle <jeff.li029@gmail.com>
2014-07-13 19:53:17 -0400

    added to create action to create vendor entry in quickbooks online and in app

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,4 +1,5 @@
 class VendorsController < ApplicationController
+  before_action :set_qb_service, only: [:create, :edit, :update, :destroy]
   def index
     @vendors = Vendor.all
   end
@@ -12,13 +13,19 @@ class VendorsController < ApplicationController
   end
 
   def create
-    @vendor = Vendor.create(vendor_params)
+    @vendor = Vendor.new(vendor_params)
+    vendor = Quickbooks::Model::Vendor.new
+    vendor.given_name = vendor_params[:name]
+    vendor.email_address = vendor_params[:email]
+    @vendor_service.create(vendor)
+
     respond_to do |format|
       if @vendor.save
         format.html { redirect_to @vendor, notice: 'Vendor was successfully created.' }
         format.json { render action: 'show', status: :created, location: @vendor }
       else
-        render action: 'new'
+        format.html { render action: 'new' }
+        format.json { render json: @vendor.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -64,7 +71,18 @@ class VendorsController < ApplicationController
   end
 
   private
+    def set_qb_service
+      oauth_client = OAuth::AccessToken.new($qb_oauth_consumer, session[:token], session[:secret])
+      @vendor_service = Quickbooks::Service::Vendor.new
+      @vendor_service.access_token = oauth_client
+      @vendor_service.company_id = session[:realm_id]
+    end
+
+    def set_vendor
+      @vendor = Vendor.find(params[:id])
+    end
+
     def vendor_params
-      params.require(:vendor).permit(:name)
+      params.require(:vendor).permit(:name, :email)
     end
 end
