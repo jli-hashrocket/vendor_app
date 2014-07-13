23eaf58d25c7fec9d82f13cbe93ae2dc88a898b7
macheteshuffle <jeff.li029@gmail.com>
2014-07-13 17:42:23 -0400

    corrected request token path

--- config/initializers/quickbooks.rb
+++ config/initializers/quickbooks.rb
@@ -3,7 +3,7 @@ QB_SECRET = "jRcCcIjSsgFOoowGhOHL72Hpvn7RSXWR26TzqXfG"
 
 $qb_oauth_consumer = OAuth::Consumer.new(QB_KEY, QB_SECRET, {
   site: "https://oauth.intuit.com",
-  request_token_path: "/oauth/v1//get_request_token",
+  request_token_path: "/oauth/v1/get_request_token",
   authorize_url: "https://appcenter.intuit.com/Connect/Begin",
   access_token_path: "/oauth/v1/get_access_token"
 })
